28aae46e5a0f0a770c41602886fd185d53d3667c
Rahul Rudra <rahulrudra665@gmail.com>
2023-05-11 17:00:33 +0600

    setup commit

---
+++ FrontEnd/reactprojects/src/Pages/Firebase/FirebaseInit.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBv0nnYqyoYjAukILjKtkJTNs_xvbtt410",
+  authDomain: "geniusshop-2f14c.firebaseapp.com",
+  projectId: "geniusshop-2f14c",
+  storageBucket: "geniusshop-2f14c.appspot.com",
+  messagingSenderId: "566552329808",
+  appId: "1:566552329808:web:1120316d228048aed8db55"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
