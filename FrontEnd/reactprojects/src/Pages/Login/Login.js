906957dbd97642fc4ad0619e39954c7c0b02f4f7
Rahul Rudra <rahulrudra665@gmail.com>
2023-05-13 10:35:13 +0600

    signup dome

--- FrontEnd/reactprojects/src/Pages/Login/Login.js
+++ FrontEnd/reactprojects/src/Pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const {
@@ -9,57 +10,64 @@ const Login = () => {
   } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
-    <div className=" h-[800px] flex justify-center items-center">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            type="email"
-            {...register("email", {
-              required: "Email is Required",
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                message: "Email must be Strong"
-              }
-            })}
-            placeholder="First name"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>{" "}
-        <br />
-        {errors.email && (
-          <span className="text-red-700">{errors.email.message}</span>
-        )}
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            type="password"
-            {...register("password", {
-              required: "Password is Required",
-              minLength: {
-                value: 5,
-                message: "Minimum Length of Password is 5"
-              },
-              pattern: {
-                // value: /^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$/,
-                message: "Password doesn't match pattern"
-              }
-            })}
-            placeholder="Enter Password"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>{" "}
-        <br />
-        {errors.password && (
-          <span className="text-red-700">{errors.password.message}</span>
-        )}
-        <br />
-        <button className="btn btn-info">Submit</button>
-      </form>
+    <div>
+      <div className=" h-[800px] flex justify-center items-center">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2 className=" text-center">Login Here</h2>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              {...register("email", {
+                required: "Email is Required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Email must be Strong"
+                }
+              })}
+              placeholder="First name"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>{" "}
+          <br />
+          {errors.email && (
+            <span className="text-red-700">{errors.email.message}</span>
+          )}
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password is Required",
+                minLength: {
+                  value: 5,
+                  message: "Minimum Length of Password is 5"
+                },
+                pattern: {
+                  // value: /^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$/,
+                  message: "Password doesn't match pattern"
+                }
+              })}
+              placeholder="Enter Password"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>{" "}
+          <br />
+          {errors.password && (
+            <span className="text-red-700">{errors.password.message}</span>
+          )}
+          <br />
+          <button className="btn btn-info">Submit</button>
+          <p>
+            Don't Have an Account
+            <Link to="/signup">Sign Up Here</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
--- FrontEnd/reactprojects/src/Pages/SignUp/SignUp.js
+++ FrontEnd/reactprojects/src/Pages/SignUp/SignUp.js
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div className="h-[800px] flex justify-center items-center">
+      {" "}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2 className="text-center">Sign Up Here</h2>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Name"
+            className="input input-bordered w-full max-w-xs"
+            {...register("name", {
+              required: "Name is Required"
+            })}
+          />
+          {errors.name && (
+            <span className="text-red-800">{errors.name.message}</span>
+          )}
+        </div>
+        <br />
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="input input-bordered w-full max-w-xs"
+            {...register("email", {
+              required: "Email is Required",
+              pattern: {
+                value: /^\S+@\S+\.\S+$/,
+                message: "Email Pattern Not Matched"
+              }
+            })}
+          />
+
+          {errors.email && (
+            <span className="text-red-800">{errors.email.message}</span>
+          )}
+        </div>
+        <br />
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            {...register(
+              "password",
+
+              {
+                required: "Password is Required",
+
+                minLength: {
+                  value: 8,
+                  message: "Min lLength is 2"
+                }
+              }
+            )}
+          />
+          {errors.password && (
+            <span className="text-red-800">{errors.password.message}</span>
+          )}
+        </div>
+        <button className="btn btn-info">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
